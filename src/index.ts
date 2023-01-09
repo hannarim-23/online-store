cbcb612a7a84d82b6ece90abb356cde9efa1dcf3
LisaDobruk <liskam8525gmail.com>
2023-01-09 16:27:15 +0100

    fix: change routing

--- src/index.ts
+++ src/index.ts
@@ -64,100 +64,26 @@ const regex2 = /(\d+)*/;
 let m: RegExpExecArray|null;
 
 export const renderUI = () => {
-    let route = routes.find((route) => window.location.pathname.includes(route.path));
+	let url = window.location.pathname;
+	console.log(url);
+    //let route = routes.find((route) => window.location.pathname.includes(route.path));
+	if(url === '/cart'){
+		routes[1].template();
+	}else if(url.includes('/product')){
+		routes[2].template();
+	}else if(url === '/'){
+		routes[3].template();
+	}else{
+		routes[0].template();
+	}
 	
-    if (route) {
+    /*if (route) {
         route.template();
     } else {
         routes[0].template();
-    }
+    }*/
 };
 
 window.addEventListener('popstate', renderUI);
 
 window.addEventListener('DOMContentLoaded', renderUI);
-/*const str = window.location.href;
-		if ((m = regex.exec(str)) !== null) {
-
-			if(!isNaN(Number(m[4]))){
-			   const path = window.location.pathname;
-			   let id = Number(path.split('/')[2]);
-
-			   routes[2].template();
-			}}else{*/
-/*function router (event: Event|string) {
-	if(event instanceof Event){
-		event.preventDefault();
-	if (event.target instanceof HTMLAnchorElement) {
-		console.log(event.target);
-		console.log(event);
-		let href = event.target.href;
-		console.log('href=', href);
-		let idOfProduct = event.target.id;
-		localStorage.setItem('idOfProduct', idOfProduct);
-		history.pushState({}, '', href);
-		//window.dispatchEvent(new Event('popstate'));
-	}
-}
-	let route = routes.find(route => route.path === window.location.pathname);
-	if (route) {
-		let html = (route.template)();
-		//window.dispatchEvent(new Event('popstate'));
-	}
-}
-router(window.location.pathname);
-document.addEventListener('click', (event: Event)=>{
-	if(event.target && event.target instanceof HTMLAnchorElement && event.target.closest("[data-link]")){
-         router(event);
-	}
-})
-//console.log('route.path', route!.path);
-	/*const str = window.location.href;
-	if ((m = regex.exec(str)) !== null) {
-		let s = regex2.exec(m[4]);
-		console.log("s", s);
-		if(regex2.exec(m[4])){
-			
-			const idOfProduct = Number(m[4]);
-		   console.log('idOfProduct', idOfProduct);
-		   console.log("m", m);
-		   //product();
-		}*/
-
-/*window.addEventListener('popstate', function() { 
-	if(mainContainer) mainContainer.innerHTML = '';
-	let route = routes.find(route => route.path === window.location.pathname); 
-	console.log(route);
-	if (route) {
-		if(mainContainer) mainContainer.innerHTML = '';
-		let html = (route.template)();
-	}
-});*/
-/*const str = window.location.href;
-m = regex.exec(str)
-if(m) {
-	const pathN = m[3];
-route = routes.find((route) => pathN);
-console.log('route', route);
-}*/
-// The result can be accessed through the `m`-variable.
-//m.forEach((match, groupIndex) => {
-//	console.log(`Found match, group ${groupIndex}: ${match}`);
-//	});
-/*window.addEventListener('DOMContentLoaded', function() { 
-	let route = routes.find(route => route.path == window.location.pathname);//защита от обновлений
-	console.log(route);
-	if (route) {
-		let html = (route.template)();
-	}
-});*/
-
-
-// Alternative syntax using RegExp constructor
-// const regex = new RegExp('(http[s]?:\\/\\/)?([^\\/\\s]+\\/)(.*)', '')
-
-
-
-
-
-
